2ac1590d0121a4f1c6812b5aa22fdd4821403d71
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-04-24 15:55:18 +0200

    java

---
+++ 03-algorithme/Java/src/introduction/App.java
@@ -0,0 +1,44 @@
+package introduction;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		// Affiche Hello World ! dans la console !!
+		
+	System.out.println("Hello World !");
+	
+	
+	// VARIABLES
+	
+	String prenom; // Déclaration d'une variable " prenom" de type "chaine de caractères"
+	
+	int age; // Déclaration d'une variable "age" de type "Nombre Entier 32 bits" (Valeur Scalaire) 
+	// (plage de valeur d'un INT - 2 147 000 000 à + 2 147 000 000)
+	
+	long soldeBancaire;  // Déclaration d'une variable "soldeBancaire" de type "Nombre Entier 64 bits" (Valeur Scalaire)
+	
+	float tailleEnMetres; // Déclaration d'une variable "tailleEnMetres" de type "Nombre réel 32 bits" (Valeur Scalaire)
+	
+	float tailleEnMetresPourLesTresGrands; // Déclaration d'une variable "tailleEnMetrespourLesTresGrands" de type "Nombre réel 64 bits" (Valeur Scalaire)
+	
+	Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen" (Valeur Scalaire)
+	
+	prenom = "Léo"; // Affectation d'une valeur à la variable "prenom"
+	
+	System.out.println(prenom);
+	
+	prenom = "juju"; // Modification de la valeur de la variable "prenom"
+	
+	System.out.println("bonjour " + prenom);
+	
+	
+	
+	
+			
+			
+	
+	
+	}
+
+}
