81e19aa2d93f60c03967e8317e3d3b665c7048c2
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-04-24 16:32:17 +0200

    Fin de l'exercice

---
+++ 03-algorithme/Java/src/helloYou/src/helloYou/App.java
@@ -0,0 +1,29 @@
+package helloYou;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	String prenom; // Déclaration d'une variable "prenom" de type "chaine de cractère"
+	
+	Scanner sc; // Déclaration d'une variable de type Scanner
+	
+	sc = new Scanner(System.in); // initialisation d'un nouveau scanner
+	
+	System.out.println("Bonjour, veuillez saisir votre prénom :"); // Affichage
+	
+	prenom = sc.nextLine(); // Demande à l'utilistauer une saisie.
+	// Lorsque l'utilisateur validera sur la touche Entrée, la saisie est récupérée dans la variable à gauche du signe.
+	
+	System.out.println("Bonjour " + prenom); // Affichage 
+	
+	sc.close(); // fermeture du scanner (OBLIGATOIRE)
+	
+	
+	
+		
+	}
+
+}
