9a7de193ea32ba4b776f833b121813d3655de55c
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-04-25 12:25:59 +0200

    Exercice 1.4

---
+++ 03-algorithme/Java/src/aireEtVolume/src/README.md
@@ -0,0 +1,29 @@
+##Exercice 1.4 : Calcul de l’aire et du volume d’une sphère
+
+Lire le rayon R d’une sphère puis calculer et afficher :
+
+Son aire = 4 π R² Son volume = (4 π R³)/3
+
+```
+
+VARIABLES
+
+rayon est un NOMBRE REEL
+
+aireRayon est NOMBRE REEL
+
+volumeRayon est un NOMBRE REEL
+
+TRAITEMENT
+
+ECRIRE "Veuillez saisir le rayon d'une sphère" LIRE rayon
+
+aireRayon <-- 4 pi rayon^2
+
+volumeRayon <-- (4 pi rayon^3) / 3
+
+RESULTAT
+
+ECRIRE "L'aire d'une sphère est de", aireRayon, "et le volume d'une sphère est de", volumeRayon, "."
+ 
+ ```
---
+++ 03-algorithme/Java/src/aireEtVolume/src/calculAireEtVolumeAire/App.java
@@ -0,0 +1,31 @@
+package calculAireEtVolumeAire;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		double rayon;
+		double aireRayon;
+		double volumeRayon;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir le rayon d'une sphère : ");
+		
+		rayon = sc.nextDouble();
+		
+		aireRayon = 4.0 * Math.PI * Math.pow(rayon, 2);
+		
+		volumeRayon = 4.0 * Math.PI * Math.pow(rayon, 3) / 3;
+		
+		System.out.println("L'aire d'une spère est de " + aireRayon + " et le volume d'une spère est de " + volumeRayon);
+				
+				
+		
+	}
+
+}
