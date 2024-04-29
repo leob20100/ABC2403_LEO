d4b1000b98df7deb284fe97d93cac7dbc2854701
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-04-29 11:35:56 +0200

    correction app

--- 03-algorithme/Java/src/aireEtVolume/src/secteurCirculaire/src/inversionValeur/src/App.java
+++ 03-algorithme/Java/src/aireEtVolume/src/secteurCirculaire/src/inversionValeur/src/App.java
@@ -29,7 +29,7 @@ public class App {
 		System.out.println("Le contenu A est " + nombreB + " ." +  " \nLe contenu A est transmis dans le contenu C qui est de " + nombreC + " \net le contenu B qui est de " + nombreA + " \nest transmis dans le contenu A qui est maintenat de " + nombreA + " . " +  " \nLa valeur du contenu A est maintenant  de " + nombreA + " \net la valeur du contenu B est maintenant de " + nombreB);
 		
 		
-		
+		sc.close();
 		
 		
 		
