f1852d9e9f4b98b8b0282e2af99bed25031f231d
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-05-21 14:51:31 +0200

    exercice 3.7

--- 03-algorithme/Java/src/etLePlusGrand/src/App.java
+++ 03-algorithme/Java/src/etLePlusGrand/src/App.java
@@ -3,26 +3,35 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-int nombre;
-int i = 1;
-int pg = 0;
-int ipg = i;
-Scanner sc = new Scanner(System.in);
+		int nombre;
+		
+		int max = 0;
+		int imax = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez le nombre numero 1 : ");
+		max = sc.nextInt();
+		boolean changement = false;
+		for (int i = 1; i < 20; i++) {
+			System.out.println("Entrez le nombre numero : "+ (i+1));
+			nombre = sc.nextInt();
 
-for (i = 1; i<20; i++) {
-	System.out.println("Entrez un nombre : ");
-	nombre = sc.nextInt();
-	
-	if(i == 1 || nombre > pg) {
-		pg = nombre;
-		ipg = i;
-	}
-	System.out.println("Le plus grand des nombres saisis est : " + pg + " . ");
-	System.out.println(pg + " était le nombre numéro " + ipg + " . ");
-	
+			if (nombre > max) {
+				max = nombre;
+				imax = i+1;
+				changement = true;
+			}
+		} if (changement == true) {
+			System.out.println("Le plus grand des nombres saisis est : " + max + " . ");
+			System.out.println(max + " était le nombre numéro " + imax + " . ");
+		}
+		else {
+			System.out.println("Le plus grand des nombres saisis est : " + max + " . ");
+			System.out.println(max + " était le nombre numéro 1. ");
+		}
+		
+
 	sc.close();
-}
-}
-	
+		
+	}
 
 }
--- 03-algorithme/Java/src/etLePlusGrandEst...Encore/src/App.java
+++ 03-algorithme/Java/src/etLePlusGrandEst...Encore/src/App.java
@@ -4,24 +4,32 @@ public class App {
 	public static void main(String[] args) {
 	int nombre = 1;
 	int i = 0;
-	int pg = 0;
-	int ipg = i ;
+	int max = 0;
+	int imax;
 	Scanner sc = new Scanner(System.in);
 	System.out.println("Entrez un nombre : ");
-	nombre = sc.nextInt();
-while(nombre <=  20 || nombre > 0) {
 	
-	i = i + 1;
-	i++;
-	if (i == 1 || nombre > pg) {
-		pg = nombre;
-		ipg = i;
-	}
-	System.out.println("Le nombre le plus grand était : " + pg);
-	System.out.println("Il a été saisi en position numéro : " + ipg);
+	nombre = sc.nextInt();
+	do {
+		i = i + 1;
+		i++;
+		if (nombre > max) {
+			max = nombre;
+			imax = i;	
+		}
+	} while (nombre != 0);
+	
+	System.out.println("Le nombre le plus grand était : " + max);
+	System.out.println(max + " était le nombre numéro 1. ");
+
+	
+	
+
+
+	
 	
 	sc.close();
 }
-	}
+	
 
 }
