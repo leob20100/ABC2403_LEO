5f8dff6032aa65426a8cf503fe6511bf90cf9a54
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-05-23 08:17:31 +0200

    exercice 3.8

--- 03-algorithme/Java/src/paiementEtRenduMonnaie/src/App.java
+++ 03-algorithme/Java/src/paiementEtRenduMonnaie/src/App.java
@@ -1,8 +1,34 @@
-
+import java.util.Scanner;
 public class App {
-
+	final static double piece1 = 0.10;
+	final static double piece2 = 0.20;
+	final static double piece3 = 0.50;
+	final static double piece4 = 1.00;
+	final static double piece5 = 2.00;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double montant;
+		double sommeDue;
+		double montantVersé;
+		double reste;
+		int billet5Euros;
+		double pieces;
+		Scanner sc = new Scanner(System.in);
+		montant = 1;
+		sommeDue = 0;
+		
+		while(montant < 0 || montant > 0) {
+			System.out.println("Saisissez le montant : ");
+			montant = sc.nextDouble();
+			sommeDue = sommeDue + montant;
+		}
+		System.out.println("Vous devez : " + sommeDue + " euros. ");
+		System.out.println("Montant versé : ");
+		montantVersé = sc.nextDouble();
+		reste = montantVersé - sommeDue;
+		System.out.println("Rendu monnaie : " + reste + " euros. ");
+		
+		
+		
 
 	}
 
