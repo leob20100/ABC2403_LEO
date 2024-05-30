3a1a390b8ebce763431b2fe777ae463caadf60cf
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-05-30 11:26:24 +0200

    Exercice n°2

--- 03-algorithme/Java/src/ExerciceTableauN°2/src/lesFruitsEtLegumes/App.java
+++ 03-algorithme/Java/src/ExerciceTableauN°2/src/lesFruitsEtLegumes/App.java
@@ -1,9 +1,32 @@
 package lesFruitsEtLegumes;
-
+import java.util.*;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nomLegume;
+		double prixLegume;
+		
+		while (true) {
+			System.out.println("Saisissez le nom du légume : ");
+			nomLegume = sc.nextLine();
+			
+			if(nomLegume.equalsIgnoreCase("go")) {
+				break;
+			}
+		}
+		
+		String legumeLeMoinsCher = null;
+		double prixLeMoinsCher;
+		double prix;
+		
+		if(prix < prixLeMoinsCher) {
+			
+			prixLeMoinsCher = prix;
+			legumeLeMoinsCher =;
+		}
 
 	}
 
