7c714e031a28e5d10e0c8488dd3e3558cc8f0ff3
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-05-30 16:41:28 +0200

    Exercice 4.3 bis

--- 03-algorithme/Java/src/Exercice 4.3 bis/src/alimenterUnTableau/App.java
+++ 03-algorithme/Java/src/Exercice 4.3 bis/src/alimenterUnTableau/App.java
@@ -1,10 +1,43 @@
 package alimenterUnTableau;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int tailleTableau;
+		int [] tableau;
+		int i;
+		int positif;
+		int negatif;
+		
+		positif = 0;
+		negatif = 0;
+		
+		System.out.println("Entrez le nombre de valeurs que vous souhaitez saisir: ");
+		
+		tailleTableau = sc.nextInt();
+		tableau = new int[tailleTableau];
+		for(i = 0; i < tailleTableau ; i++) {
+			
+			System.out.println("Saisissez la valeur N° " + (i+1) + " / " + tailleTableau);
+			tableau[i] = sc.nextInt();
+			
+			if(tableau[i] >= 0) {
+				positif++;
+			
+			}
+			else {
+				negatif++;
+			}
+		}
+		System.out.println("Vous avez saisi : " + positif + " nombres positifs");
+		System.out.println("Vous avez saisi : " + negatif + " nombres negatifs");
+			
+		
 	}
 
 }
