fac61311cada8abedee32dfd792f239db9e0932b
leob20100 <166002478+leob20100@users.noreply.github.com>
2024-05-31 08:09:57 +0200

    exercice 4.5 bis

--- 03-algorithme/Java/src/Exercice 4.5 bis/src/sommeDesValeursDe2Tableaux/App.java
+++ 03-algorithme/Java/src/Exercice 4.5 bis/src/sommeDesValeursDe2Tableaux/App.java
@@ -3,8 +3,37 @@ package sommeDesValeursDe2Tableaux;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] tableau1; // Déclaration du 1er tableau
+		int[] tableau2; // Déclaration du 2nd tableau
+		int resultatTableau; // résultat du calcul
+		
+		resultatTableau = 0; // Initialisation du résultat à 0
+		tableau1 = new int[] { 4, 8, 7, 12 }; // Initialisation du 1er tableau
+		tableau2 = new int[] { 3, 6 }; // Initialisation du 2nd tableau
+		
+		/**
+		 * POUR i de 0 à tableau1.length-1, incrémenter i
+		 * 		POUR j de 0 à tableau2.length-1, incrémenter j
+		 * 			resultatTableau = resultatTableau + tableau1[i] * tableau2[j]
+		 * 		FIN POUR
+		 * FIN POUR
+		 */
+		for(int i = 0; i < tableau1.length; i++) {
+			
+			for(int j = 0; j < tableau2.length; j++) {
+				resultatTableau += tableau1[i] * tableau2[j];
+			}
+			
+		}
+		
+		System.out.println(resultatTableau);
 
 	}
+
+
+		
+		
+	
 
 }
